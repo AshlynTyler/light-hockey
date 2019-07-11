9e93e77d8f269f6c8ca9f22def16f0ed4480d481
Cyber Witch <no-reply>
2019-07-10 19:51:12 -0600

    added code to make each player's disk follow them.

--- client/src/GameScreen.js
+++ client/src/GameScreen.js
@@ -34,7 +34,7 @@ function Disk(x,y){
 
     this.y = y
 
-    this.maxSpeed = 10;
+    this.maxSpeed = 20;
 
     this.radius = 20;
 }
@@ -51,6 +51,24 @@ function UnclaimedDisk(x,y){
 //returns the distance between two points.
 function distance(x1,y1,x2,y2){
     return Math.sqrt(((x1-x2)**2)+((y1-y2)**2))
+}
+
+//returns the angle of direction in radians from the first point to the second point
+// relative to the positive x axis (going counterclockwise).
+
+function direction(x1,y1,x2,y2){
+
+    return Math.atan2(y1-y2,x2-x1)
+}
+
+//returns the x and y length of a right triangle with hypotenuse of "length"
+// and an angle of "angle".
+
+function findXY(length,angle){
+    return{
+        y: Math.sin(angle) * length * -1,
+        x: Math.cos(angle) * length
+    }
 }
 
 //hello
@@ -70,7 +88,7 @@ class GameScreen extends React.Component {
         
 
         gameInterval = setInterval(() =>{
-            this.renderFrame()
+            this.gameStep()
         },(1000/60))
 
         socket = this.props.userSocket
@@ -120,6 +138,40 @@ class GameScreen extends React.Component {
         )
     }
 
+    gameStep = function(){
+
+        if (typeof(thisId) != "undefined"){
+
+            let player = players[thisId]
+
+            //moves your disk towards you.
+
+            if(player.hasDisk === 1){
+
+                if(distance(player.mouse.x,player.mouse.y,player.disk.x,player.disk.y) <= player.disk.maxSpeed){
+                    players[thisId].disk.x = player.mouse.x
+                    players[thisId].disk.y = player.mouse.y
+                }
+                else{
+                    const moveDist = findXY(player.disk.maxSpeed,direction(player.disk.x,player.disk.y,player.mouse.x,player.mouse.y))
+
+                    console.log(moveDist)
+
+                    players[thisId].disk.x += moveDist.x
+                    players[thisId].disk.y += moveDist.y
+                }
+
+            }
+
+            socket.emit("player info", players[thisId])
+
+        }
+
+        
+
+        this.renderFrame()
+    }
+
     renderFrame = function(){
 
         
@@ -130,6 +182,8 @@ class GameScreen extends React.Component {
 
         for(let i = 0; i < players.length; i++){
 
+                //render mouse pointer of players
+
                 draw.beginPath();
 
                 draw.arc(players[i].mouse.x,players[i].mouse.y,5,0,2*Math.PI)
@@ -137,6 +191,8 @@ class GameScreen extends React.Component {
                 draw.fillStyle = players[i].color
         
                 draw.fill();
+
+                //render disks of players
 
                 if(players[i].hasDisk === 1){
 
@@ -149,6 +205,8 @@ class GameScreen extends React.Component {
                     draw.stroke();
                 }
         }
+
+        //render unclaimed disks
 
         for(let i = 0; i < unclaimedDisks.length; i++){
 
@@ -171,7 +229,7 @@ class GameScreen extends React.Component {
             players[thisId].mouse.x = event.pageX - canvasPos.offsetLeft
             players[thisId].mouse.y = event.pageY - canvasPos.offsetTop
 
-            socket.emit("player info", players[thisId])
+            
         }
     }
 
@@ -181,8 +239,6 @@ class GameScreen extends React.Component {
         if(players[thisId].hasDisk === 0){
 
             unclaimedDisks.forEach(function(current){
-
-                console.log(distance(players[thisId].mouse.x,players[thisId].mouse.y,current.x,current.y))
 
                 if(distance(players[thisId].mouse.x,players[thisId].mouse.y,current.x,current.y) <= current.radius){
                     players[thisId].disk = new Disk(current.x,current.y)
