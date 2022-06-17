64b3a2d546d4cd25a9e10a8d5df14930320e92f9
Cyber Witch <46911274+AshlynTyler@users.noreply.github.com>
2022-06-17 17:35:58 -0600

    bleh

--- client/src/GameScreen.js
+++ client/src/GameScreen.js
@@ -509,6 +509,8 @@ class GameScreen extends React.Component {
     renderFrame = function(){
 
         draw.setTransform((this.refs.canvas.offsetWidth / 1200), 0, 0, (this.refs.canvas.offsetHeight / 675), 0, 0)
+        console.log(this.refs.canvas.offsetWidth)
+        console.log(this.refs.canvas.offsetHeight)
     
         this.clearCanvas();
 
